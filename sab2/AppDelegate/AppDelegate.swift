e902328b086e5e28163d51a1b149c52f2e06a3b6
Esraa Abuseada <esraa.hamada@ibtikar.net.sa>
2019-10-17 16:21:18 +0200

    tabbbar with controller

--- sab2/AppDelegate/AppDelegate.swift
+++ sab2/AppDelegate/AppDelegate.swift
@@ -17,6 +17,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+       /// da yetfasaaaaaal
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window!.backgroundColor = UIColor.white
+        let rootViewController = TabBarViewController()
+       // let navigationController = UINavigationController(rootViewController: rootViewController)
+        window?.rootViewController = rootViewController
+        window!.makeKeyAndVisible()
         return true
     }
 
